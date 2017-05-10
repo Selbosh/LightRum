a147b9bf6767555f206f39999966ff27b23f5ca3
Selby <david@selbys.org.uk>
2017-05-10 18:05:27 +0100

    Polished flex layout for desktop viewports

--- content.php
+++ content.php
@@ -2,7 +2,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <div class="article-inner">
     <?php if ( has_post_thumbnail() ):
-      the_post_thumbnail( 'medium' );
+      the_post_thumbnail( 'large' );
     endif; ?>
     <header class="entry-header">
       <?php the_title(
