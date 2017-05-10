a46ba73ff23a98a1d529cf997b1745804305fd01
Selby <david@selbys.org.uk>
2017-05-10 17:49:40 +0100

    Initial basic homepage layout

--- content.php
+++ content.php
@@ -1,15 +1,20 @@
 <!-- template for post excerpts on archive pages -->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <header class="entry-header">
-    <?php the_title(
-      sprintf('<h1 class="entry-title"><a href="%s">', esc_url(get_permalink()) ),
-      '</a></h1>'
-    ); ?>
-  </header>
-  <section class="entry-content">
-    Arty McArticle, Conti McContent.
-  </section>
-  <footer class="entry-footer">
-    Footy McFooter
-  </footer>
+  <div class="article-inner">
+    <?php if ( has_post_thumbnail() ):
+      the_post_thumbnail( 'medium' );
+    endif; ?>
+    <header class="entry-header">
+      <?php the_title(
+        sprintf('<h1 class="entry-title"><a href="%s">', esc_url(get_permalink()) ),
+        '</a></h1>'
+      ); ?>
+    </header>
+    <section class="entry-summary">
+      <?php the_excerpt(); ?>
+    </section>
+    <footer class="entry-footer">
+      Footy McFooter
+    </footer>
+  </div><!--.article-inner-->
 </article>
