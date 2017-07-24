93b9ecc34a37870a36bab9f78aad59120a29dc80
Selbosh <david@selbys.org.uk>
2017-07-24 19:42:05 +0100

    Basic check for categories

--- content.php
+++ content.php
@@ -4,10 +4,14 @@
   <div class="article-inner">
     <a href="<?php echo esc_url(get_permalink()); ?>">
     <header class="entry-header <?php if ($thumb) { echo 'thumb'; ?>"
-      style="background-image: linear-gradient(rgba(255, 255, 255, .3), rgba(255, 255, 255, .3)), url(<?php echo $thumb; } ?>">
+      style="background-image: linear-gradient(rgba(255, 255, 255, .2), rgba(255, 255, 255, .2)), url(<?php echo $thumb; } ?>">
     <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
   </header></a>
     <?php echo '<time>'.get_the_date( get_option('date_format') ).'</time>'; ?>
+    <?php if ( in_category(array('News', 'Reviews', 'Tutorials')) ) {
+      // https://developer.wordpress.org/reference/functions/get_categories/
+      echo 'I am news, reviews or tutorials';
+    } ?>
 
     <div class="entry-summary">
       <?php the_excerpt(); ?>
