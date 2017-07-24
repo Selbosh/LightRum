fc65130ce3ef9ff6ac1ccc72eeb679381912ef3b
Selbosh <david@selbys.org.uk>
2017-07-24 18:14:59 +0100

    Text on images using flexbox

--- content.php
+++ content.php
@@ -2,15 +2,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <?php $thumb = get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>
   <div class="article-inner">
-    <?php if ($thumb): ?>
-    <header class="entry-header featured-thumb" style="background-image: url(<?php echo $thumb; ?>);">
-    <?php else: ?>
-    <header class="entry-header">
-    <?php endif; ?>
-      <?php the_title(
-        sprintf('<h1 class="entry-title"><a href="%s">', esc_url(get_permalink()) ),
-        '</a></h1>'
-      ); ?>
+    <header class="entry-header <?php if ($thumb) { echo 'thumb'; ?>"
+      style="background-image: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url(<?php echo $thumb; } ?>">
+    <?php the_title(
+      sprintf('<h1 class="entry-title"><a href="%s">', esc_url(get_permalink()) ), '</a></h1>');
+      ?>
     </header>
     <?php echo '<time>'.get_the_date( get_option('date_format') ).'</time>'; ?>
 
