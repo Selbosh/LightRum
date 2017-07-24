08713404d5751cae23780d60214fae9562b05778
Selbosh <david@selbys.org.uk>
2017-07-24 16:54:16 +0100

    Titles appear over featured images

--- content.php
+++ content.php
@@ -2,15 +2,18 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <?php $thumb = get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>
   <div class="article-inner">
-    <?php if ( has_post_thumbnail() ):
-      the_post_thumbnail( 'medium' );
-    endif; ?>
+    <?php if ($thumb): ?>
+    <header class="entry-header featured-thumb" style="background-image: url(<?php echo $thumb; ?>);">
+    <?php else: ?>
     <header class="entry-header">
+    <?php endif; ?>
       <?php the_title(
         sprintf('<h1 class="entry-title"><a href="%s">', esc_url(get_permalink()) ),
         '</a></h1>'
       ); ?>
     </header>
+    <?php the_date('', '<time>', '</time>'); ?>
+
     <div class="entry-summary">
       <?php the_excerpt(); ?>
     </div>
