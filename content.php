0339d261afee3b6a03be14755c8f2b5efba03f59
Selbosh <david@selbys.org.uk>
2017-07-24 18:42:43 +0100

    Make whole thumbnail clickable

--- content.php
+++ content.php
@@ -2,12 +2,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <?php $thumb = get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>
   <div class="article-inner">
+    <a href="<?php echo esc_url(get_permalink()); ?>">
     <header class="entry-header <?php if ($thumb) { echo 'thumb'; ?>"
-      style="background-image: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url(<?php echo $thumb; } ?>">
-    <?php the_title(
-      sprintf('<h1 class="entry-title"><a href="%s">', esc_url(get_permalink()) ), '</a></h1>');
-      ?>
-    </header>
+      style="background-image: linear-gradient(rgba(255, 255, 255, .6), rgba(255, 255, 255, .8)), url(<?php echo $thumb; } ?>">
+    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+  </header></a>
     <?php echo '<time>'.get_the_date( get_option('date_format') ).'</time>'; ?>
 
     <div class="entry-summary">
