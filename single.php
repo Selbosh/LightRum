1cc1fc7af857f5529f80b889bd57505ffea566dd
Selbosh <david@selbys.org.uk>
2017-07-24 12:44:53 +0100

    Prettify author URLs and move post nav above comments

--- single.php
+++ single.php
@@ -31,19 +31,31 @@
         </div>
         <address class="author-title"><?php the_author(); ?></address>
         <p class="author-description"><?php the_author_meta('description'); ?></p>
-        <?php if ( get_the_author_meta('user_url') ): ?>
-          <p class="author-url">Web site: <a href="<?php echo esc_url( get_the_author_meta('user_url') ); ?>"><?php the_author_meta('user_url'); ?></a></p>
+        <?php if ( get_the_author_meta('user_url') ):
+          $pretty_author_url = get_the_author_meta('user_url');
+          $url_prefixes = array( 'http://', 'https://', 'www.' );
+          foreach ($url_prefixes as $item) {
+            $pretty_author_url = str_replace($item, '', $pretty_author_url);
+          } ?>
+          <p class="author-url">
+            <!--Web site:-->
+            <a href="<?php echo esc_url( get_the_author_meta('user_url') ); ?>">
+              <?php echo $pretty_author_url; ?>
+            </a>
+          </p>
         <?php endif; ?>
       </footer>
 
     </article>
 
-      <?php // If comments are open or we have at least one comment, load up the comment template.
+      <?php the_post_navigation( array (
+        'prev_text' => 'Previous post: %title',
+        'next_text' => 'Next post: %title'
+      ) );
+      /* If comments are open or we have at least one comment, load up the comment template. */
       if ( comments_open() || get_comments_number() ):
         comments_template();
       endif;
-
-      the_post_navigation();
 
   endwhile; // End of the Loop
 ?>
