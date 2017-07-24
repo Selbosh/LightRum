5ed052cbf67635d585a4dc26f7eab9b1ced31115
Selbosh <david@selbys.org.uk>
2017-07-24 19:05:44 +0100

    headline bg no longer transparent; looks ace

--- content.php
+++ content.php
@@ -4,7 +4,7 @@
   <div class="article-inner">
     <a href="<?php echo esc_url(get_permalink()); ?>">
     <header class="entry-header <?php if ($thumb) { echo 'thumb'; ?>"
-      style="background-image: linear-gradient(rgba(255, 255, 255, .6), rgba(255, 255, 255, .8)), url(<?php echo $thumb; } ?>">
+      style="background-image: linear-gradient(rgba(255, 255, 255, .3), rgba(255, 255, 255, .3)), url(<?php echo $thumb; } ?>">
     <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
   </header></a>
     <?php echo '<time>'.get_the_date( get_option('date_format') ).'</time>'; ?>
