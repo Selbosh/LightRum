13af54565a5e6f1a1dc46224b3ad562109bc0d75
Selbosh <david@selbys.org.uk>
2017-07-25 18:32:58 +0100

    Remove borders from linked images

--- functions.php
+++ functions.php
@@ -92,6 +92,26 @@ function disable_wp_emojicons() {
 add_action( 'init', 'disable_wp_emojicons' );
 
 /*
+===================
+Links with images
+===================
+*/
+function give_linked_images_class($content) {
+  // see https://stackoverflow.com/a/24042991
+  $classes = 'img-link'; // separate classes by spaces - 'img image-link'
+  // check if there are already a class property assigned to the anchor
+  if ( preg_match('/<a.*? class=".*?"><img/', $content) ) {
+    // If there is, simply add the class
+    $content = preg_replace('/(<a.*? class=".*?)(".*?><img)/', '$1 ' . $classes . '$2', $content);
+  } else {
+    // If there is not an existing class, create a class property
+    $content = preg_replace('/(<a.*?)><img/', '$1 class="' . $classes . '" ><img', $content);
+  }
+  return $content;
+}
+add_filter('the_content', 'give_linked_images_class');
+
+/*
 =====================
 Remove <head> junk
 =====================
