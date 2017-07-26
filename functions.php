5673e560aa5323c721410cbb89a27fd6af7fe2b7
Selbosh <david@selbys.org.uk>
2017-07-26 10:00:46 +0100

    Add HTML5 support

--- functions.php
+++ functions.php
@@ -20,6 +20,7 @@ function lightrum_setup() {
   add_theme_support('post-thumbnails');
   add_theme_support('automatic-feed-links');
   add_theme_support('title-tag');
+  add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
   add_theme_support('custom-logo', array(
     'height' => 72,
     'width'  => 400,
