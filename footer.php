c9e689e1157399f6467ea4a0fe219166375782f1
Selbosh <david@selbys.org.uk>
2017-07-26 09:46:41 +0100

    Improve footer widget spacing

--- footer.php
+++ footer.php
@@ -4,9 +4,11 @@
     'menu_id'        => 'footer-menu',
   ) ); ?>
   <?php get_template_part( 'menu', 'social' ); ?>
-  <?php if ( is_active_sidebar('footer_1') ):
-    dynamic_sidebar( 'footer_1' );
-  endif; ?>
+  <?php if ( is_active_sidebar('footer_1') ): ?>
+    <div id="footer-widget-area">
+      <?php dynamic_sidebar( 'footer_1' ); ?>
+    </div>
+  <?php endif; ?>
   <?php wp_footer(); ?>
 </footer>
 
