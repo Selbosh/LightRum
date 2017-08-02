1fda49081c625fc7ff791d3c0aca648cbc18bbd9
Selby <david@selbys.org.uk>
2017-08-02 16:31:18 +0100

    Add Facebook Likes to post indices

--- header.php
+++ header.php
@@ -6,6 +6,16 @@
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
+    <!--Facebook SDK -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.10";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    <!--/Facebook SDK-->
     <header>
       <?php if ( has_custom_logo() ):
         the_custom_logo();
