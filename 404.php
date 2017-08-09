008435677c0b7fe657eb4deb9b67644bc91a292b
Selbosh <david@selbys.org.uk>
2017-08-09 10:06:16 +0100

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+  <main role="main">
+    <h1>Page not found</h1>
+    <p>Unfortunately, the page you requested could not be found.</p>
+  </main>
+
+<?php get_footer(); ?>
