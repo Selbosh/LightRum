a2df0fbe1037eeaa77a3a735d97a0ee0bb437e12
Selbosh <david@selbys.org.uk>
2017-08-11 12:19:12 +0100

    Move favicon code to <head>

--- header.php
+++ header.php
@@ -4,6 +4,14 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <?php wp_head(); ?>
+    <!--Favicons-->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+    <!--/Favicons-->
   </head>
   <body <?php body_class(); ?>>
     <!--Facebook SDK-->
@@ -30,14 +38,6 @@
       };
       return t;
     }(document, "script", "twitter-wjs"));</script>
-    <!--Favicons-->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="theme-color" content="#ffffff">
-    <!--/Favicons-->
     <header>
       <?php if ( has_custom_logo() ):
         the_custom_logo();
